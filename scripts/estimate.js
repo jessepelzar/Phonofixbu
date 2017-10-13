33c7acb6018ee3258b6dda375754707837c090a3
jessepelzar <jessepelzar@gmail.com>
2017-10-13 11:09:52 -0400

    started estimate script

---
+++ scripts/estimate.js
@@ -0,0 +1,19 @@
+
+
+var cracked = document.getElementById('cracked_screen');
+var battery = document.getElementById('battery_dead');
+var other = document.getElementById('other');
+
+var iPhone_7p = document.getElementById('model_7+');
+var iPhone_7 = document.getElementById('model_7');
+var iPhone_6sp = document.getElementById('model_6s+');
+var iPhone_6s = document.getElementById('model_6s');
+var iPhone_6p = document.getElementById('model_6+');
+var iPhone_6 = document.getElementById('model_6');
+var iPhone_5se = document.getElementById('model_5se');
+var iPhone_5s = document.getElementById('model_5s');
+var iPhone_5c = document.getElementById('model_5c');
+var iPhone_5 = document.getElementById('model_5');
+
+
+const prices = ["50", "50", "60", "60", "60", "70", "70", "80", "100", "120"];
